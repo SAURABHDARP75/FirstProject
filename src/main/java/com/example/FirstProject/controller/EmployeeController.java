70397b5b8387300e0caba4fdd3874aff3fb665f8
saurabhdarp75 <darpsaurabh@gmail.com>
2023-08-11 11:57:40 +0530

    api info

---
+++ src/main/java/com/example/FirstProject/controller/EmployeeController.java
@@ -0,0 +1,15 @@
+package com.example.FirstProject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeController {
+
+	@GetMapping("/info")
+	public String ApiInfo() {
+		return "API is Running";
+	}
+}
